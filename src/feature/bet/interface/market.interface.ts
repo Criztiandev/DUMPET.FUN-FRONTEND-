8349859048c0e58e4af1242d3a2d9b535bfe90d9
criztian <criztiandev@gmail.com>
2024-11-13 16:26:26 +0800

    chores: rename proper naming for market interface

---
+++ src/feature/bet/interface/market.interface.ts
@@ -0,0 +1,11 @@
+export interface Market {
+  BlockHeight: string;
+  Creator: string;
+  Duration: string;
+  OptionA: string;
+  OptionB: number;
+  ProcessId: number;
+  Timestamp: number;
+  Title: string;
+  TokenTxId: string;
+}
