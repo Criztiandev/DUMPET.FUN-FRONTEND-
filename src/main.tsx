46567ece17519e27a8994edba8a4b93b08ef8e38
criztian <criztiandev@gmail.com>
2024-11-13 16:20:54 +0800

    chores: move the theme provider at the top and added toast provider

--- src/main.tsx
+++ src/main.tsx
@@ -15,41 +15,45 @@ import ErrorScreen from "./common/components/page/helper/error-screen/index.tsx"
 import { ThemeProvider } from "./common/components/template/provider/theme-provider/index.tsx";
 // @ts-ignore
 import { ArweaveWalletKit } from "arweave-wallet-kit";
+import { Toast, ToastProvider } from "./common/components/atoms/ui/toast.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Suspense fallback={<LoadingScreen />}>
-        <QueryErrorResetBoundary>
-          {({ reset }) => (
-            <ErrorBoundary
-              onReset={reset}
-              fallbackRender={({ resetErrorBoundary }) => (
-                <ErrorScreen onReset={resetErrorBoundary} />
-              )}
-            >
-              <ArweaveWalletKit
-                config={{
-                  permissions: ["SIGN_TRANSACTION", "ACCESS_ADDRESS"],
-                  appInfo: {
-                    name: "Super Cool App",
-                    logo: "https://arweave.net/jAvd7Z1CBd8gVF2D6ESj7SMCCUYxDX_z3vpp5aHdaYk",
-                  },
-                  ensurePermissions: true,
-                }}
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <Suspense fallback={<LoadingScreen />}>
+          <QueryErrorResetBoundary>
+            {({ reset }) => (
+              <ErrorBoundary
+                onReset={reset}
+                fallbackRender={({ resetErrorBoundary }) => (
+                  <ErrorScreen onReset={resetErrorBoundary} />
+                )}
               >
-                <ThemeProvider>
-                  <SidebarProvider>
-                    <App />
-                  </SidebarProvider>
-                </ThemeProvider>
-              </ArweaveWalletKit>
-            </ErrorBoundary>
-          )}
-        </QueryErrorResetBoundary>
-      </Suspense>
-    </QueryClientProvider>
+                <ArweaveWalletKit
+                  config={{
+                    permissions: ["SIGN_TRANSACTION", "ACCESS_ADDRESS"],
+                    appInfo: {
+                      name: "Super Cool App",
+                      logo: "https://arweave.net/jAvd7Z1CBd8gVF2D6ESj7SMCCUYxDX_z3vpp5aHdaYk",
+                    },
+                    ensurePermissions: true,
+                  }}
+                >
+                  <ToastProvider>
+                    <SidebarProvider>
+                      <App />
+                      <Toast />
+                    </SidebarProvider>
+                  </ToastProvider>
+                </ArweaveWalletKit>
+              </ErrorBoundary>
+            )}
+          </QueryErrorResetBoundary>
+        </Suspense>
+      </QueryClientProvider>
+    </ThemeProvider>
   </StrictMode>
 );
