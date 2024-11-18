a11b8c116aa887014bdaae3ff832313d757588f2
criztian <criztiandev@gmail.com>
2024-11-19 07:33:18 +0800

    refactor: remove uncessary variant config

--- src/common/components/atoms/button/profile-button/index.tsx
+++ src/common/components/atoms/button/profile-button/index.tsx
@@ -1,14 +1,9 @@
 import { Button } from "../../ui/button";
 import { User } from "lucide-react";
-import { useIsMobile } from "@/hooks/use-mobile";
 
 const ProfileButton = () => {
-  const isSmallScreen = useIsMobile();
   return (
-    <Button
-      className="w-full space-x-2"
-      variant={isSmallScreen ? "outline" : "default"}
-    >
+    <Button className="w-full space-x-2" variant="ghost">
       <User size={18} />
       <span>Profile</span>
     </Button>
