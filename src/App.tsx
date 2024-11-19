8935a87cad9ffccdb2ac927cba4756e0ca5ef5f4
criztian <criztiandev@gmail.com>
2024-11-19 10:55:02 +0800

    refactor: theme update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import userRoutes from "./feature/user/user.routes";
+import { useTheme } from "@/common/components/template/provider/theme-provider";
+import { useEffect } from "react";
 
 const App = () => {
+  const { setTheme } = useTheme();
   const routes = userRoutes;
+
+  useEffect(() => {
+    setTheme("dark");
+  }, []);
   return <RouterProvider router={routes} />;
 };
 
