5f5f642f092316b6127a61f90356be80dadaeb96
criztian <criztiandev@gmail.com>
2024-11-25 09:48:46 +0800

    Convert to native one

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,41 @@ import userRoutes from "./feature/user/user.routes";
 import { useTheme } from "@/common/components/template/provider/theme-provider";
 import { useEffect } from "react";
 
+import { HashRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import MainScreen from "./feature/user/pages/main-screen";
+import ProtectedRoutes from "./common/components/template/hoc/ProtectedRoutes";
+import ProfileScreen from "./feature/user/pages/profile-screen";
+import PublicProfileScreen from "./feature/user/pages/public-profile-screen";
+import DetailsScreen from "./feature/user/pages/details-screen";
+import CreateMarketScreen from "./feature/user/pages/create-market-screen";
+
 const App = () => {
   const { setTheme } = useTheme();
-  const routes = userRoutes;
 
   useEffect(() => {
     setTheme("dark");
   }, []);
 
-  return <RouterProvider router={routes} />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path={"/"} element={<MainScreen />} />
+        <Route path={"/home"} element={<MainScreen />} />
+        <Route
+          path={"/account"}
+          element={
+            <ProtectedRoutes>
+              <ProfileScreen />
+            </ProtectedRoutes>
+          }
+        />
+        <Route path={"/profile/:id"} element={<PublicProfileScreen />} />
+        <Route path={"/market/:id"} element={<DetailsScreen />} />
+        <Route path={"/create/market"} element={<CreateMarketScreen />} />
+      </Routes>
+    </HashRouter>
+  );
 };
 
 export default App;
