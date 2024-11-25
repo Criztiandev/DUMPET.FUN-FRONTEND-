3bb86e1e10a83bb0a256ef334e94d31b9f9b26f5
criztian <criztiandev@gmail.com>
2024-11-26 01:39:25 +0800

    feat: pause conclude for testing

--- src/common/components/atoms/button/conclude-button/index.tsx
+++ src/common/components/atoms/button/conclude-button/index.tsx
@@ -1,17 +1,17 @@
 import { Button } from "../../ui/button";
 import useMarketStore from "@/feature/market/store/market.store";
 import useMarketConclude from "@/feature/market/hooks/market/user-market-conclude";
-import { isMarketConcluded } from "@/common/utils/time.utilts";
+// import { isMarketConcluded } from "@/common/utils/time.utilts";
 const ConcludeButton = () => {
   const { selectedMarket } = useMarketStore();
   const { mutate, isPending } = useMarketConclude(
     String(selectedMarket?.MarketInfo.ProcessId) || ""
   );
-  const currentDate = new Date();
-  const isConcluded = isMarketConcluded(
-    currentDate,
-    Number(selectedMarket?.MarketInfo.Duration)
-  );
+  // const currentDate = new Date();
+  // const isConcluded = isMarketConcluded(
+  //   currentDate,
+  //   Number(selectedMarket?.MarketInfo.Duration)
+  // );
 
   const handleConclude = () => {
     mutate();
@@ -22,7 +22,7 @@ const ConcludeButton = () => {
       <Button
         onClick={handleConclude}
         className="relative"
-        disabled={!isConcluded || isPending}
+        disabled={isPending}
       >
         {isPending ? "Loading..." : <span>Conclude</span>}
       </Button>
